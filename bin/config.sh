85db52b52aaef9ca3c71415799feca22020f852d
Nico Haenggi <nico@haenggi.ch>
2020-11-29 15:00:59 +0100

    created config script

---
+++ bin/config.sh
@@ -0,0 +1,112 @@
+#!/bin/bash
+
+###############################################################################
+# Set variables                                                               #
+###############################################################################
+
+BIN=~/dotfiles/bin                # shell scripts
+CONFIG=~/dotfiles/config          # configuration files directory
+SETUP=~/dotfiles                  # root folder of dotfiles
+
+###############################################################################
+# Configure                                                                   #
+###############################################################################
+
+# Entering as Root
+printf "Enter root password...\n"
+sudo -v
+
+# Keep alive Root
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
+# Install Oh-My-Zsh [1/3]
+printf "📦 Install Zsh...\n"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# Install Zsh plugins [2/3]
+printf "📦 Install Zsh plugins...\n"
+git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-completions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+# Update Zsh settings [3/3]
+printf "⚙️ Update Zsh settings...\n"
+sudo rm -rf ~/.zshrc > /dev/null 2>&1
+cp $CONFIG/.zshrc ~/.zshrc
+
+# Update Git settings [1/1]
+printf "⚙️ Update Git settings...\n"
+sudo rm -rf ~/.gitconfig > /dev/null 2>&1
+sudo rm -rf ~/.gitignore > /dev/null 2>&1
+cp $CONFIG/.gitignore ~/.gitignore
+cp $CONFIG/.gitconfig ~/.gitconfig
+
+# Configure macOS Finder
+printf "⚙️ Configure Finder...\n"
+defaults write -g AppleShowAllExtensions -bool true
+defaults write com.apple.finder AppleShowAllFiles true
+defaults write com.apple.finder _FXShowPosixPathInTitle -bool YES
+defaults write com.apple.finder ShowPathbar -bool true
+chflags nohidden ~/Library
+/System/Library/Frameworks/CoreServices.framework/Frameworks/LaunchServices.framework/Support/lsregister -kill -r -domain local -domain system -domain user
+
+# Configure macOS Screen Capture
+printf "⚙️ Save screenshots in PNG format...\n"
+mkdir ~/Pictures/Screenshots
+defaults write com.apple.screencapture location -string "~/Pictures/Screenshots"
+defaults write com.apple.screencapture type -string "png"
+
+# Configure macOS Keyboard
+printf "⚙️ Configure Keyboard...\n"
+defaults write -g NSAutomaticSpellingCorrectionEnabled -bool false
+defaults write -g NSAutomaticPeriodSubstitutionEnabled -bool false
+defaults write -g NSAutomaticDashSubstitutionEnabled -bool false
+defaults write -g NSAutomaticQuoteSubstitutionEnabled -bool false
+defaults write com.apple.messageshelper.MessageController SOInputLineSettings -dict-add "continuousSpellCheckingEnabled" -bool false
+
+# Configure macOS Safari
+printf "⚙️ Configure Safari...\n"
+defaults write com.apple.Safari UniversalSearchEnabled -bool false
+defaults write com.apple.Safari SuppressSearchSuggestions -bool true
+defaults write com.apple.Safari WebAutomaticSpellingCorrectionEnabled -bool false
+defaults write com.apple.Safari AutoOpenSafeDownloads -bool false
+defaults write com.apple.Safari ShowFavoritesBar -bool true
+defaults write com.apple.Safari IncludeDevelopMenu -bool true
+
+# Configure macOS TextEdit
+printf "⚙️ Configure TextEdit...\n"
+defaults write com.apple.TextEdit RichText -int 0
+defaults write com.apple.TextEdit PlainTextEncoding -int 4
+defaults write com.apple.TextEdit PlainTextEncodingForWrite -int 4
+
+# Configure macOS Trackpad
+printf "⚙️ Configure Trackpad...\n"
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
+defaults -currentHost write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
+defaults write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
+defaults write NSGlobalDomain com.apple.swipescrolldirection -bool false
+
+# Configure macOS
+printf "⚙️ Various configuration...\n"
+defaults write com.apple.gamed Disabled -bool true
+sudo /usr/libexec/ApplicationFirewall/socketfilterfw --setglobalstate on
+defaults write com.apple.TimeMachine DoNotOfferNewDisksForBackup -bool true
+
+# Create Projects directory
+printf "⚙️ Create Projects directory...\n"
+mkdir ${HOME}/Projects
+chmod 777 ${HOME}/Projects
+
+# Check if Python3 is installed via Homebrew
+if brew ls --versions python3 > /dev/null; then
+  brew uninstall --ignore-dependencies python3
+else
+  echo "Python3 is not installed! Install it from https://www.python.org"
+fi
+
+# Cleanup
+printf "⚙️ Cleanup and final touches...\n"
+brew -v update && brew -v upgrade && brew cask upgrade && mas upgrade && brew -v cleanup --prune=2 && brew doctor
+
+# Exit script
+exit
